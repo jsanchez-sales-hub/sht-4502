cdc24edb0cef4bbeaadb27a16e6be4430d076012
Julio Sanchez de la Vega <jsanchez@sales-hub.com>
2024-08-12 15:54:17 -0700

    First draft

--- check-for-second-attempts.js
+++ check-for-second-attempts.js
@@ -19,25 +19,106 @@ const asyncMain = async () => {
 	const cards_info_arr = csvToArray(cards_info_csv);
 	const total = cards_info_arr.length;
 
+	const new_cards_info_arr = [];
+
 	for (let [index, card_info] of cards_info_arr.entries()) {
+		const {
+			run_id,
+			order_id,
+			timestamp,
+			cardNumber,
+			expirationDate,
+			cvv,
+			lastKnownIp,
+			trucentiveLink,
+			balance
+		} = card_info;
 		console.log(
-			`Checking element ${index + 1} of ${total}: Card ${card_info.cardNumber}`
+			`Checking element ${index + 1} of ${total}: Card ${cardNumber}`
 		);
 
-		const card_number = card_info.cardNumber;
-
-		const grep_command = `grep "${card_number}" ${all_time_log_path}`;
+		let grep_command = `grep "${cardNumber}" ${all_time_log_path}`;
 
 		/**
+		 * Grep result as string with "\n" characters.
 		 * @type { string }
 		 */
-		const grep_result = await execute(grep_command);
+		let grep_result = await execute(grep_command);
 
-		console.log(typeof grep_result);
+		const grep_arr = grep_result
+			? grep_result.split('\n').map(r => {
+					try {
+						return JSON.parse(r);
+					} catch (err) {
+						return {};
+					}
+				})
+			: [];
 
-		console.log(grep_result);
-		break;
+		/**
+		 * Run ID's that at some point contain this card number
+		 */
+		const run_ids_set = new Set();
+		// Will start from this card_info's run_id and not before, since it's chronological
+		const first_run_id = grep_arr.findIndex(g => g.runId === run_id);
+		for (let i = first_run_id + 1; i < grep_arr.length; i++) {
+			const log = grep_arr[i];
+			if (log.runId) {
+				if (log.runId === run_id) continue;
+
+				run_ids_set.add(log.runId);
+			}
+		}
+
+		// No other Run ID mentioned this card number; skip.
+		if (run_ids_set.size === 0) {
+			console.log(`No other Run ID's show this card number. Continuing.`);
+			continue;
+		}
+
+		// Look for each of the Run ID's in the all-time.log file.
+		grep_command = `grep `;
+		for (const this_run_id of run_ids_set) {
+			grep_command += `-e "${this_run_id}" `;
+		}
+		grep_command += ` ${all_time_log_path}`;
+		const logs_of_interest = await execute(grep_command);
+		// Parse them into JSON.
+		const logs_of_interest_arr = logs_of_interest
+			? logs_of_interest.split('\n').map(r => {
+					try {
+						return JSON.parse(r);
+					} catch (err) {
+						return {};
+					}
+				})
+			: [];
+
+		const successful_attempt = logs_of_interest_arr.find(log => {
+			return log.msg === 'Response from payOnLandingPagePnm' && log.isSuccess;
+		});
+
+		if (successful_attempt) {
+			console.log(
+				`Run ID ${successful_attempt.runId} was actually successful with this one. Removing it from new array.`
+			);
+			continue;
+		}
+
+		console.log(
+			`Card Number was not successfully used after all it seems. Adding it to new array.`
+		);
+		new_cards_info_arr.push(card_info);
 	}
+
+	/**
+	 * CSV-formatted string of final cards info array.
+	 * @type { string }
+	 */
+	const new_cards_info_csv = arrayToCSV(new_cards_info_arr);
+
+	Fs.writeFileSync(output_path, new_cards_info_csv);
+	console.log(`Finished Unused Cards Report - Check for second attempts.`);
 };
 
 asyncMain()
