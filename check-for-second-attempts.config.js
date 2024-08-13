f453d077c135b33e3fbcb0b051d40ba038acdd10
Julio Sanchez de la Vega <jsanchez@sales-hub.com>
2024-08-13 11:37:46 -0700

    Restores ecosystem config file

--- check-for-second-attempts.config.js
+++ check-for-second-attempts.config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	apps: [
 		{
-			name: 'SHT-4502 | Check for Second Attempts | Parallel',
+			name: 'SHT-4502 | Check for Second Attempts',
 			script: './check-for-second-attempts.js',
 			autorestart: false,
 			time: true,
-			log_file: './pm2/check-for-second-attempts-parallel-out.log'
+			log_file: './pm2/check-for-second-attempts-out.log'
 		}
 	]
 };
